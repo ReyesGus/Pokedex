9a6b5300410ca8d83bbd26be3b95006c5849f271
ReyesGus <50606224+ReyesGus@users.noreply.github.com>
2020-04-26 23:42:56 -0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:pokedex/pokedetail.dart';
+import 'package:pokedex/pokemon.dart';
+import 'dart:convert';
+
+void main() => runApp(MaterialApp(
+  title: "Pokemon App",
+  home: HomePage(),
+  theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Carter'),
+  debugShowCheckedModeBanner: false,
+));
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var url =
+      "https://raw.githubusercontent.com/Ignacio246/sexta_generacion/master/pokemones.json";
+  PokeHub pokeHub;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  void fetchData() async {
+    var res = await http.get(url);
+    var decodedValue = jsonDecode(res.body);
+    pokeHub = PokeHub.fromJson(decodedValue);
+    setState(() {});
+  }
+
+  /*Control de Busqueda*/
+  String _searchText = "";
+  final TextEditingController _search = new TextEditingController();
+  Widget _appBarTitle = new Text("Pokemon");
+  bool _typing = false;
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: _typing ? TextField(
+          autofocus: true,
+          controller: _search,
+          onChanged: (text){
+            setState(() {});
+          },
+        ):Text("Pokemon"),
+        leading: IconButton(
+          icon: Icon(_typing ? Icons.done: Icons.search),
+          onPressed: () {
+            print("Is typing: "+ _typing.toString());
+            setState(() {
+              _typing = !_typing;
+              _search.text = "";
+            });
+          },
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: pokeHub == null
+          ? Center(
+        child: CircularProgressIndicator(),
+      )
+          : OrientationBuilder(
+        builder: (context, orientation) {
+          return GridView.count(
+            crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
+            children: pokeHub.pokemon
+                .where((poke) =>
+            ((poke.name
+                .toLowerCase()
+                .contains(_search.text.toLowerCase())) ||
+                poke.type.toList().toList().toString()
+                    .toLowerCase()
+                    .contains(_search.text.toLowerCase()) ||
+                poke.num.toString().contains(_search.text)
+            ))
+                .map((Pokemon poke) =>
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PokeDetail(
+                                pokemon: poke,
+                              )));
+                    },
+                    child: Card(
+                      elevation: 3.0,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Hero(
+                            tag: poke.img,
+                            child: Container(
+                              height: 100.0,
+                              width: 100.0,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: NetworkImage(poke.img)),
+                              ),
+                            ),
+                          ),
+                          Text(
+                            poke.name,
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ))
+                .toList(),
+          );},),
+      endDrawer: Drawer(
+        elevation: 16.0,
+        child: ListView(
+          children: const <Widget>[
+            DrawerHeader(
+              child: Text("Pokemon App",
+                  style: TextStyle(fontFamily: 'Carter', fontSize: 30)),
+              decoration: BoxDecoration(color: Colors.greenAccent),
+            ),
+            ListTile(
+              title: Text('Search by Name'),
+              leading: Icon(Icons.assignment_ind)
+            ),
+            ListTile(
+                title: Text('Search by Num'),
+                leading: Icon(Icons.plus_one)
+            ),
+            ListTile(
+                title: Text('Search by Wearness'),
+                leading: Icon(Icons.all_inclusive)
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
